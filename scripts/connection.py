77ca27f4dade8406cb69a0044a414a10ea3ac99d
Ethel <etheltibeb@gmail.com>
2024-09-05 23:19:29 +0300

    User behvi.overview and Exploratory data analysis

--- scripts/connection.py
+++ scripts/connection.py
@@ -16,7 +16,6 @@ class DatabaseConnector:
         self.connection = None
 
     def connect(self):
-
         try:
             self.connection = psycopg2.connect(
                 dbname=self.dbname,
@@ -29,15 +28,14 @@ class DatabaseConnector:
         except Exception as e:
             print(f"An error occurred while connecting to the database: {e}")
 
-    def execute_query(self, query):
-    
+    def execute_query(self, query, params=None):
         if self.connection is None:
             print("No connection to the database.")
             return None
 
         try:
             with self.connection.cursor() as cursor:
-                cursor.execute(query)
+                cursor.execute(query, params)
                 results = cursor.fetchall()
                 colnames = [desc[0] for desc in cursor.description]
                 df = pd.DataFrame(results, columns=colnames)
@@ -45,6 +43,20 @@ class DatabaseConnector:
         except Exception as e:
             print(f"An error occurred while executing the query: {e}")
             return None
+
+    def execute_update(self, query, params=None):
+        """For INSERT, UPDATE, DELETE queries."""
+        if self.connection is None:
+            print("No connection to the database.")
+            return
+
+        try:
+            with self.connection.cursor() as cursor:
+                cursor.execute(query, params)
+                self.connection.commit()  # Commit changes to the database
+                print("Query executed and committed successfully.")
+        except Exception as e:
+            print(f"An error occurred during update: {e}")
 
     def close_connection(self):
         if self.connection is not None:
@@ -55,3 +67,4 @@ class DatabaseConnector:
                 print(f"An error occurred while closing the connection: {e}")
         else:
             print("No connection to close.")
+
